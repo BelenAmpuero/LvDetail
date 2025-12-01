d2dffcdd7e42f6f6ad29b822b50d92092351501b
BelenAmpuero <bnampuero@gmail.com>
2025-12-01 20:41:54 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./componentes/navbar"
+
+function App() {
+  return (
+    <main>
+    <Navbar />
+          <div>App</div>
+</main>
+  )
+}
+
+export default App
---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar (){
+    return (
+        <header className='p-3 flex justify-between'>
+            <h1 className='text-3xl'>LvDetail</h1>
+            <nav className='flex gap-5'>
+                <a href="">Productos</a>
+                <a href="">Contacto</a>
+            </nav>
+            <button></button>
+        </header>
+    );
+}
+
+export default Navbar
