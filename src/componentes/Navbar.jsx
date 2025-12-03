b31e66c8a65a5700ca14c6e69a7f40f0e26164e9
BelenAmpuero <bnampuero@gmail.com>
2025-12-03 13:44:29 -0300

    Commit solo de archivos modificados

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,7 +1,10 @@
+import logo from '../assets/logoSinFondo.png';
+
 function Navbar (){
     return (
         <header className='p-3 flex justify-between'>
             <h1 className='text-3xl'>LvDetail</h1>
+            <img src={logo} className='4px'/>
             <nav className='flex gap-5'>
                 <a href="">Productos</a>
                 <a href="">Contacto</a>
